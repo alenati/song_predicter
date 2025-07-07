495c325bfb7d2783a08a4bcb2268386d90822547
alenati <alanabluecake@gmail.com>
2025-07-07 23:02:26 +0200

    feat: predictive model is done

--- song_predicter/main.py
+++ song_predicter/main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv("D:/song_predicter/data/Spotify_Dataset_V3.csv", sep = ';')
+
+
 print(df.iloc[:5,4:10])
 #print(df.columns)
 #print(df.info())
@@ -20,3 +24,21 @@ df[scaling_table] = X_scaled_df
 print(df.iloc[:5,4:10])
 
 df.to_csv("D:/song_predicter/data/Spotify_Dataset_V3_scaled.csv", sep=';', index=False)
+
+X = df[scaling_table]
+Y = df['Continent']
+
+X_train, X_test, Y_train, Y_test = train_test_split (X,Y, test_size = 0.2, random_state = 42)
+
+model = RandomForestClassifier(random_state = 42)
+model.fit(X_train, Y_train)
+
+accuracy = model.score(X_test, Y_test)
+print(accuracy)
+
+from sklearn.metrics import classification_report
+
+Y_pred = model.predict(X_test)
+print(classification_report(Y_test, Y_pred))
+
+
